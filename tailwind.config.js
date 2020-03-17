3b8e4336cdba2b5bba152544c1b632ec42001b77
Aldo Dumitrescu <aldodumitrescu@gmail.com>
2020-03-17 18:31:21 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Cerebri Sans', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: theme => ({
+        outline: '0 0 0 2px ' + theme('colors.indigo.500'),
+      }),
+      fill: theme => theme('colors'),
+    },
+  },
+  variants: {
+    fill: ['responsive', 'hover', 'focus', 'group-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    zIndex: ['responsive', 'focus'],
+  },
+  plugins: [],
+}
